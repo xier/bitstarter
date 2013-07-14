0551ec6e2e38d603d48cdb4a3ac946e80ba937e2
Xi-Er Dang <xier89@hotmail.com>
2013-07-14 20:59:57 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
-fs=require('fs');
-var buf = new Buffer(size, encoding);
-var buf = new Buffer(fs.readFileSync('file.html'), 'utf-8');
-var data = fs.readFileSync("index.html"), buffer.toString("utf-8"));
-response.send(data);
-  });
+var express = require('express');
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
-
