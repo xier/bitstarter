bc1ade52f4010d57f17e065cc9e562ee6a23aedd
Xi-Er Dang <xier89@hotmail.com>
2013-07-14 20:56:55 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+fs=require('fs');
+var buf = new Buffer(size, encoding);
+var buf = new Buffer(fs.readFileSync('file.html'), 'utf-8');
+var data = fs.readFileSync("index.html"), buffer.toString("utf-8"));
+response.send(data);
+  });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
