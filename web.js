3ec3c1568a496f3f931b55c6832b459253029b1e
xier <xier.dang@hotmail.com>
2013-08-07 19:29:33 +0000

    Updated product description.

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var buffer = new Buffer(fs.readFileSync("index.html"), 'utf 8'); 
  response.send(buffer.toString())});
  
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 console.log("Listening on " + port);
 });
